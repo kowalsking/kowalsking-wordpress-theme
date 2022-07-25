5f1b9bd68f88cefa5817bb4babdf55aba5322c69
kowalsking <kovvalsking@gmail.com>
2022-07-25 23:26:36 +0300

    add some script

--- functions.php
+++ functions.php
@@ -141,8 +141,6 @@ function kowalsking_scripts() {
 	wp_enqueue_style( 'kowalsking-style', get_stylesheet_uri(), array(), _S_VERSION );
 	wp_style_add_data( 'kowalsking-style', 'rtl', 'replace' );
 
-	wp_enqueue_script( 'kowalsking-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true );
-
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
---
+++ js/frontPage.js
@@ -0,0 +1,46 @@
+import gsap from 'gsap'
+
+export default class {
+  constructor () {
+    this.h1 = document.querySelector('.dmytro')
+    if (!this.h1) return
+
+    this.showH1()
+    this.colorizeH1()
+  }
+
+  showH1 () {
+    gsap.fromTo(this.h1, {
+      opacity: 0
+    }, {
+      delay: 1,
+      opacity: 0.8,
+      duration: 1.7
+    })
+  }
+
+  colorizeH1 () {
+    const dima = this.h1.innerText.split('').map(d => {
+      if (d === ' ') return document.createElement('br')
+      const element = document.createElement('span')
+      element.innerText = d
+      element.className = 'd'
+      element.style.display = 'inline-block'
+      return element
+    });
+
+    this.h1.innerText = ''
+    dima.forEach(d => this.h1.append(d))
+
+    const allD = document.querySelectorAll('.d')
+    allD.forEach(d => {
+      d.addEventListener('mouseover', () => {
+        d.style.color = `rgb(${Math.random() * 255},${Math.random() * 255},${Math.random() * 255})`
+        d.style.transform = `translate(${Math.random() * 15}px, ${Math.random() * 15}px) rotate(${Math.random() * (15 - -15 + 1) + -15}deg)`
+      })
+      d.addEventListener('mouseout', () => {
+        d.style.color = `rgb(${Math.random() * 255},${Math.random() * 255},${Math.random() * 255})`
+      })
+    })
+  }
+}
